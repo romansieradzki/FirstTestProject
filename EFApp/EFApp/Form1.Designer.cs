e0cad39c7a43ec15670280489079daea452df22f
romansieradzki <romansieradzki@o2.pl>
2014-04-27 21:09:18 +0200

    Initial start of test project

---
+++ EFApp/EFApp/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace EFApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.cbFilter = new System.Windows.Forms.CheckBox();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.cmbTitle = new System.Windows.Forms.ComboBox();
+            this.cmbCountry = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgvCustomers.Location = new System.Drawing.Point(0, 0);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.Size = new System.Drawing.Size(577, 271);
+            this.dgvCustomers.TabIndex = 0;
+            // 
+            // cbFilter
+            // 
+            this.cbFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cbFilter.AutoSize = true;
+            this.cbFilter.Location = new System.Drawing.Point(12, 290);
+            this.cbFilter.Name = "cbFilter";
+            this.cbFilter.Size = new System.Drawing.Size(48, 17);
+            this.cbFilter.TabIndex = 1;
+            this.cbFilter.Text = "Filter";
+            this.cbFilter.UseVisualStyleBackColor = true;
+            this.cbFilter.CheckedChanged += new System.EventHandler(this.cbFilter_CheckedChanged);
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(12, 335);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(35, 13);
+            this.lblCustomerName.TabIndex = 2;
+            this.lblCustomerName.Text = "Name";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtCustomerName.Location = new System.Drawing.Point(71, 328);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(121, 20);
+            this.txtCustomerName.TabIndex = 3;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(12, 362);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(27, 13);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "Title";
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblCountry.AutoSize = true;
+            this.lblCountry.Location = new System.Drawing.Point(12, 389);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(43, 13);
+            this.lblCountry.TabIndex = 5;
+            this.lblCountry.Text = "Country";
+            // 
+            // cmbTitle
+            // 
+            this.cmbTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cmbTitle.FormattingEnabled = true;
+            this.cmbTitle.Items.AddRange(new object[] {
+            "All"});
+            this.cmbTitle.Location = new System.Drawing.Point(71, 354);
+            this.cmbTitle.Name = "cmbTitle";
+            this.cmbTitle.Size = new System.Drawing.Size(121, 21);
+            this.cmbTitle.TabIndex = 6;
+            // 
+            // cmbCountry
+            // 
+            this.cmbCountry.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cmbCountry.FormattingEnabled = true;
+            this.cmbCountry.Items.AddRange(new object[] {
+            "All"});
+            this.cmbCountry.Location = new System.Drawing.Point(71, 381);
+            this.cmbCountry.Name = "cmbCountry";
+            this.cmbCountry.Size = new System.Drawing.Size(121, 21);
+            this.cmbCountry.TabIndex = 7;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRefresh.Location = new System.Drawing.Point(479, 409);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 8;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 444);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.cmbCountry);
+            this.Controls.Add(this.cmbTitle);
+            this.Controls.Add(this.lblCountry);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.lblCustomerName);
+            this.Controls.Add(this.cbFilter);
+            this.Controls.Add(this.dgvCustomers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCustomers;
+        private System.Windows.Forms.CheckBox cbFilter;
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblCountry;
+        private System.Windows.Forms.ComboBox cmbTitle;
+        private System.Windows.Forms.ComboBox cmbCountry;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ EFApp/EFApp/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EFApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayCustomers();
+            DisplayCountries();
+            DisplayTitles();
+        }
+
+        private void DisplayCustomers()
+        {
+            using (var dc = new NorthwindEntities())
+            {
+                var customers = from c in dc.Customers
+                                select new
+                                {
+                                    CusomerId = c.CustomerID,
+                                    Name = c.ContactName,
+                                    Title = c.ContactTitle,
+                                    Country = c.Country
+                                };
+
+                if (!string.IsNullOrWhiteSpace(txtCustomerName.Text))
+                    customers = customers.Where(c => c.Name.Contains(txtCustomerName.Text));
+                if (cmbTitle.SelectedIndex > 0)
+                    customers = customers.Where(c => c.Title == cmbTitle.SelectedItem );
+                if (cmbCountry.SelectedIndex > 0)
+                    customers = customers.Where(c => c.Country == cmbCountry.SelectedItem );
+                dgvCustomers.DataSource = customers;
+            }
+        }
+
+        private void DisplayCountries()
+        {
+            using (var ct = new NorthwindEntities())
+            { 
+                var countries = (from c in ct.Customers orderby c.Country select c.Country).Distinct();
+                cmbCountry.Items.AddRange(countries.ToArray());
+                cmbCountry.SelectedIndex = 0;
+            }
+        }
+
+        private void DisplayTitles()
+        {
+            using (var ct = new NorthwindEntities())
+            {
+                var titles = (from c in ct.Customers orderby c.ContactTitle select c.ContactTitle).Distinct();
+                cmbTitle.Items.AddRange(titles.ToArray());
+                cmbTitle.SelectedIndex = 0;
+            }
+        }
+
+        private void cbFilter_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayCustomers();
+        }
+
+    }
+}
